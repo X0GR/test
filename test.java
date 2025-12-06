81de639de121516f1ff8050e082025a46014080d
X0GR <gnaneshwarofficial123@gmail.com>
2025-12-06 17:12:00 +0530

    Update and rename test to test.java

---
+++ test.java
@@ -0,0 +1,11 @@
+package test;
+
+public class TEST123 {
+	
+	public class HelloWorld {
+	    public static void main(String[] args) {
+	        System.out.println("Hello, World!");
+	    }
+	}
+
+}
